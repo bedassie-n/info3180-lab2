39d001096a060e9ee4865456851eb74e20228540
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2021-02-10 12:07:02 -0500

    Finished Lab 2

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,17 @@
 /* Add your Application JavaScript */
+window.onload = () => {
+    const follow = document.querySelector('#lower > button');
+    follow.addEventListener('click', followClick, true);
+
+    function followClick(){
+        incrementFollowers();
+        follow.className = "followed";
+        follow.textContent = "Followed"
+        follow.removeEventListener('click', followClick, true);
+    }
+    
+    function incrementFollowers(){
+        var followers = document.querySelector("#followers > h3");
+        followers.innerHTML = Number(followers.textContent) + 1
+    }
+}
